b4f0f7ba1b6688c544235b2aa0b4735dad10b651
Bismark7 <bismen007@gmail.com>
2023-02-16 16:22:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/* main print string with puts()
- * return 0 */
+/**
+ * main print string with puts()
+ * return 0
+ * */
 int main(void)
 { puts("\n""Programming is like building a multilingual puzzle");
        	return(0)
