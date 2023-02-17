3b67a8230b8cccb640af539cebe40a9dae6b8d01
Bismark7 <bismen007@gmail.com>
2023-02-16 20:36:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main print string with puts()
- * return 0
+ * main- A code that print string with puts()
+ * Return: ends with 0
  */
-int main()
+int main(void)
 { puts("\"Programming is like building a multilingual puzzle");
-return(0);
+ return (0);
 }
