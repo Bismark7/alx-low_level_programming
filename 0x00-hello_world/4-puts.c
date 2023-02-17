c2b8981c3ab3204f9c04b3e19b647619d621dd3d
Bismark7 <bismen007@gmail.com>
2023-02-16 16:31:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  * return 0
  * */
 int main(void)
-{ puts("\n""Programming is like building a multilingual puzzle");
-       	return(0)
-		;}
+{ puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
