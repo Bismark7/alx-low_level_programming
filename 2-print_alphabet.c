d34fb5612aa1dbdbc5fe8fa0f02d55dd595d2cfc
Bismark7 <bismen007@gmail.com>
2023-02-17 21:43:52 -0800

    2-print_alphabet.c

---
+++ 2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * return: 0 (success)
+ */
+
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+	putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
