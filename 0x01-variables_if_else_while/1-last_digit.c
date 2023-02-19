016bfad4dddcfe1130625cdb0631724b0d0e080e
Bismark7 <bismen007@gmail.com>
2023-02-19 09:45:00 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,8 +6,9 @@
 /* betty style doc for function main goes there */
 
 /**
- * main - Entry point
- * Return : Always 0 (success)
+ * main- Entry point
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
