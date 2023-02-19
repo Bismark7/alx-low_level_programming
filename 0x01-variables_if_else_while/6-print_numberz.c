63ce897b44f854b2f255295461e9c2c496743ff4
Bismark7 <bismen007@gmail.com>
2023-02-19 13:11:45 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,9 +8,10 @@
 
 int main(void)
 {
-        int i;
-        for(i = '0';i <= '9';i++)
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
 		putchar(i + 0);
 	putchar('\n');
-        return (0);
+	return (0);
 }
