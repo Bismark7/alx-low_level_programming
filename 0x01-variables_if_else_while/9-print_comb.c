b92ec28040396f1deca0b3646b673130c7e940e9
Bismark7 <bismen007@gmail.com>
2023-02-19 15:05:01 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,6 +2,7 @@
 
 /**
  *main - Entry point
+ *
  *Return: Always 0 (seccess)
  */
 
@@ -14,6 +15,7 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
+		
 		putchar(',');
 		putchar(' ');
 	}
