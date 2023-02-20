e0c56e7aca9b2f7ee7444f668f6d9dd32e025833
Bismark7 <bismen007@gmail.com>
2023-02-20 14:33:08 -0800

    last commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,26 +8,26 @@
 
 int main(void)
 {
-	int a, b, c, d;
+int a, b, c, d;
 
-	for (a = 0; a <= 9; a++)
-		for (b = 0; b <= 9; b++)
-			for (c = 0; c <= 9; c++)
-				for (d = 0; d <= 9; d++)
-					if ((a < c) || ((a == c) && (b < d)))
-					{
-						putchar(a + '0');
-						putchar(b + '0');
-						putchar(' ');
-						putchar(c + '0');
-						putchar(d + '0');
+for (a = 0; a <= 9; a++)
+for (b = 0; b <= 9; b++)
+for (c = 0; c <= 9; c++)
+for (d = 0; d <= 9; d++)
+if ((a < c) || ((a == c) && (b < d)))
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(' ');
+putchar(c + '0');
+putchar(d + '0');
 
-						if (a != 9 || b != 8 || c != 9 || d != 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-	putchar('\n');
-	return (0);
+if (a != 9 || b != 8 || c != 9 || d != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
