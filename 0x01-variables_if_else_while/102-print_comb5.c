2d9ca7b5aba2b4bc58e518134fb2bcf39af47865
Bismark7 <bismen007@gmail.com>
2023-02-20 14:10:52 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,9 @@ int main(void)
 	int a, b, c, d;
 
 	for (a = 0; a <= 9; a++)
-	{
 		for (b = 0; b <= 9; b++)
-		{
 			for (c = 0; c <= 9; c++)
-			{
 				for (d = 0; d <= 9; d++)
-				{
 					if ((a < c) || ((a == c) && (b < d)))
 					{
 						putchar(a + '0');
@@ -32,11 +28,6 @@ int main(void)
 							putchar(' ');
 						}
 					}
-				}
-			}
-		}
-	}
-
 	putchar('\n');
 	return (0);
 }
