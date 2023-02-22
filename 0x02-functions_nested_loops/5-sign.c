6279080468cc2e794dc8ee5894ce1419d9cdb409
Bismark <bismen007@gmail.com>
2023-02-22 18:24:40 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
  /**
   *print_sign-check the sign of a number
   *@n:Number whose sign is to be checked
@@ -11,14 +12,14 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
+	}
 		else if (n < 0)
 		{
 			_putchar('-');
 			return(-1);
+		}
 			else {
 				_putchar('0');
 				return (0);
 			}
-		}
-	}
 }
