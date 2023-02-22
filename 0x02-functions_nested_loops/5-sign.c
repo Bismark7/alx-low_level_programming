e093d84036d440f53083684ab5a3affc198586cf
Bismark <bismen007@gmail.com>
2023-02-22 18:36:43 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
  /**
-  *print_sign - Print sign of a number
+  *print_sign - Entry point
   *@n: Number to check it sign
   *
   *Return: 1 if n is > 0 and 1- if n < 0  else 0 if equals 0
