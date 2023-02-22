80836d2c40afd5508485d02ba9200ec438d0c0ba
Bismark <bismen007@gmail.com>
2023-02-22 22:30:13 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - Add two integers
+ *@a: first operand to perform addition operation
+ *@b: second operand to perform addition operation
+ *
+ * Return: Add of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
