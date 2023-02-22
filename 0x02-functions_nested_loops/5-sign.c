0d8659465655e8986b24543c9c4654fa5b719aec
Bismark <bismen007@gmail.com>
2023-02-22 18:20:21 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,16 +5,20 @@
   *
   *Return: 1 if n is > 0 and 1- if n < 0  else 0 if equals 0
   */
-
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
-	else if (n < 0)
-		_putchar ('-');
-		return (-1);
-	else
-		_putchar('0');
-		return (0);
+		else if (n < 0)
+		{
+			_putchar('-');
+			return(-1);
+			else {
+				_putchar('0');
+				return (0);
+			}
+		}
+	}
 }
