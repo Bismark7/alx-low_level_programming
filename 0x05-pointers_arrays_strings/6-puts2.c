5917f6e86662c4f3f0b32075c24eef9cd56635c2
Bismark7 <bismen007@gmail.com>
2023-02-27 21:19:26 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - puts function
+ * @str: parameter for puts2 function
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
