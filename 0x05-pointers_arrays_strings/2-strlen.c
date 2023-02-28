c2063ed130925ae3aa3f92aabd7c6a6da3d01991
Bismark7 <bismen007@gmail.com>
2023-02-27 17:22:15 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *@s: parameter
+ *
+ * Return: strlen
+ */
+
+int _strlen(char *s)
+{
+	/*return (strlen(s)); */
+
+	int len;
+
+	for (; *s != '\0'; s++)
+	{
+		len += 1;
+	}
+	return (len);
+}
