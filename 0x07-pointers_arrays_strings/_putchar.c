d3764639026d02bd648db9e7da729a0ee1ad8ec2
Bismark7 <bismen007@gmail.com>
2023-03-05 07:08:12 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,4 +1,4 @@
-#include <unist.h>
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -10,5 +10,5 @@
 
 int _putchar(char c)
 {
-	return (write(1, &c, -1));
+	return (write(1, &c, 1));
 }
