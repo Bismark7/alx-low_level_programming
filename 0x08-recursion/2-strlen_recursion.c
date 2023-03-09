c6a00012ac22337094244ad495c814d66c31726e
Bismark7 <bismen007@gmail.com>
2023-03-09 07:30:36 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,6 +4,17 @@
  *_strlen_recursion - print length of a string
  *@s: string to be measured
  *
- *
- *
+ *Return: lenght
  */
+
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
