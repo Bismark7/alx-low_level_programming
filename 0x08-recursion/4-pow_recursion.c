bffcca036047222ae8552441920f28dfda747147
Bismark7 <bismen007@gmail.com>
2023-03-09 08:11:43 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  *_pow_recursion - returns the value of x raised to the power of y
- *@x:
- *@y:
+ *@x: number to be raised
+ *@y: power of a number
  *
  *Return: value of x to the power y
  */
@@ -18,7 +18,7 @@ int _pow_recursion(int x, int y)
 	else if (y == 0)
 		return (1);
 
-	value *= _pow_recursion(x, y-1);
+	value *= _pow_recursion(x, y - 1);
 
 	return (value);
 }
