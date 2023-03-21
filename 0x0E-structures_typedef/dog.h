067a64a19700090fb469ea1841f7c4b3b88bf38e
Bismark7 <bismen007@gmail.com>
2023-03-21 03:20:23 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct dog - details for poppy dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Gives the entry details of a poppy
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
