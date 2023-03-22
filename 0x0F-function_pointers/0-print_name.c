417755b9cd19463c2280fcfe632973d2b29b21e9
Bismark7 <bismen007@gmail.com>
2023-03-22 16:33:29 -0700

    main.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/*
+ *print_name - prints a name
+ *@nmae: char pointer
+ *f: void function
+ *
+ *Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
