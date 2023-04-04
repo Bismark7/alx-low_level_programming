06668bc988952b93e248072682c261a1cd26e2c6
Bismark7 <bismen007@gmail.com>
2023-04-04 14:44:41 -0700

    first commit

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - print character to stdout
+ * @c: character to print
+ *
+ * Return: on success 1
+ * on error, 1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1);
+}
