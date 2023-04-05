5b1114edfa381085a7a68f6077183320e16a6982
Bismark7 <bismen007@gmail.com>
2023-04-05 10:02:38 -0700

    update 2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_listint - print all elements of s listint_t
@@ -9,13 +10,15 @@
  */
 
 size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
 
-	h = malloc(sizeof(const listint_t);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
 
-	if (h == NULL)
-		printf("listint is empty");
-		else if (h != NULL)
-	printf("%d\n", h->n);
-	h = h->next;
-	return (h);
+	return (num);
 }
