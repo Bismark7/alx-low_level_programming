cbb4f61b0f5f6a4d4552215bf9093785c520fabc
Bismark7 <bismen007@gmail.com>
2023-04-05 10:33:21 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,9 +2,9 @@
 
 /**
  * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * @h: linked list of type listint_t to traverse reference
  *
- * Return: number of nodes
+ * Return: number
  */
 size_t listint_len(const listint_t *h)
 {
